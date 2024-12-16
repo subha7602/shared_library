27581e8269c48dc610d45477d32fe74823df74a1
Subhashini S V <subhashinisv@presidio.com>
2024-12-16 10:43:32 +0530

    second commit

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 //just give your credentialsId for the docker repo that you have saved in the Jenkins credentials manager.
 def login() {
-    withCredentials([usernamePassword(credentialsId: '8a3bb0ae-9ff1-40e0-a0da-2ef7c1dc2cad', usernameVariable: 'username', passwordVariable: 'password')]) {
+    withCredentials([usernamePassword(credentialsId: 'DockerHubCredential', usernameVariable: 'username', passwordVariable: 'password')]) {
         sh """
             docker login --username="${username}" --password="${password}"
         """
